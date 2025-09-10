7403a617b7dc0efd9ac72a62dc1c41359682032e
-akku- <akkun11.open@gmail.com>
2025-09-10 22:26:11 +0900

    pstore: init MediaTek ramconsole driver

---
+++ fs/pstore/mtk_ram.c
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-2.0-only
+
+#include <linux/module.h>
+// #include <linux/of.h>
+// #include <linux/platform_device.h>
+
+// #include <linux/pstore.h>
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("akku <akkun11.open@gmail.com>");
+MODULE_DESCRIPTION("Pstore for MediaTek RAM Console");
