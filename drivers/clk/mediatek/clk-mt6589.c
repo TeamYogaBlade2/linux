9ac77afeb1e7bcf8cdd16c117c53705cb0501b55
-akku- <akkun11.open@gmail.com>
2025-09-23 23:24:20 +0900

    WIP: clk: mediatek: mt6589-topckgen init muxs

--- drivers/clk/mediatek/clk-mt6589.c
+++ drivers/clk/mediatek/clk-mt6589.c
@@ -12,18 +12,77 @@
 
 
 /* TOPRGU - topckgen? */
+/*
+ * FIXME: are the regs correct?
+ *	> #define CLK_CFG_0           (TOPRGU_BASE + 0x0140)
+ *	but many SoCs use 0x0040 as CLK_CFG_0
+ * FIXME: ops and flags
+ */
 
 static struct mtk_composite top_muxes[] = {
-	/* CLK_CFG_0 */
-	/* CLK_CFG_1 */
-	/* CLK_CFG_2 */
-	/* CLK_CFG_3 */
-	/* CLK_CFG_4 */
-	/* CLK_CFG_5 */
-	/* CLK_CFG_6 */
-	/* CLK_CFG_7 */
-	/* CLK_MISC_CFG_2 */
-	/* CLK_CFG_8 */
+	// /* CLK_CFG_0 */
+	// MUX_GATE(CLK_TOP_MUX_MFG, "mfg_sel", mfg_parents,
+	// 	0x0140, 16, 3, 23),
+	// MUX_GATE(CLK_TOP_MUX_IRDA, "irda_sel", irda_parents,
+	// 	0x0140, 24, 2, 31),
+	//
+	// /* CLK_CFG_1 */
+	// MUX_GATE(CLK_TOP_MUX_CAM, "cam_sel", cam_parents,
+	// 	0x0144, 0, 4, 7),
+	// MUX_GATE(CLK_TOP_MUX_AUDINTBUS, "audintbus_sel", audintbus_parents,
+	// 	0x0144, 8, 2, 15),
+	// MUX_GATE(CLK_TOP_MUX_JPG, "jpg_sel", jpg_parents,
+	// 	0x0144, 16, 3, 23),
+	// MUX_GATE(CLK_TOP_MUX_DISP, "disp_sel", disp_parents,
+	// 	0x0144, 24, 3, 31),
+	//
+	// /* CLK_CFG_2 */
+	// MUX_GATE(CLK_TOP_MUX_MSDC1, "msdc1_sel", msdc1_parents,
+	// 	0x0148, 0, 3, 7),
+	// MUX_GATE(CLK_TOP_MUX_MSDC2, "msdc2_sel", msdc2_parents,
+	// 	0x0148, 8, 3, 15),
+	// MUX_GATE(CLK_TOP_MUX_MSDC3, "msdc3_sel", msdc3_parents,
+	// 	0x0148, 16, 3, 23),
+	// MUX_GATE(CLK_TOP_MUX_MSDC4, "msdc4_sel", msdc4_parents,
+	// 	0x0148, 24, 3, 31),
+	//
+	// /* CLK_CFG_3 */
+	// MUX_GATE(CLK_TOP_MUX_USB20, "usb20_sel", usb20_parents,
+	// 	0x014C, 0, 2, 7),
+	//
+	// /* CLK_CFG_4 */
+	// MUX_GATE(CLK_TOP_MUX_HYD, "hyd_sel", hyd_parents,
+	// 	0x0150, 0, 3, 7),
+	// MUX_GATE(CLK_TOP_MUX_VENC, "venc_sel", venc_parents,
+	// 	0x0150, 8, 3, 15),
+	// MUX_GATE(CLK_TOP_MUX_SPI, "spi_sel", spi_parents,
+	// 	0x0150, 16, 3, 23),
+	// MUX_GATE(CLK_TOP_MUX_UART, "uart_sel", uart_parents,
+	// 	0x0150, 24, 2, 31),
+	//
+	// /* CLK_CFG_6 */
+	// MUX_GATE(CLK_TOP_MUX_CAMTG, "camtg_sel", camtg_parents,
+	// 	0x0158, 8, 3, 15),
+	// /*
+	// MUX_GATE(CLK_TOP_MUX_FD, "fd_sel", fd_parents,
+	// 	0x0158, 16, 3, 23),
+	// */
+	// MUX_GATE(CLK_TOP_MUX_AUDIO, "audio_sel", audio_parents,
+	// 	0x0158, 24, 2, 31),
+	//
+	// /* CLK_CFG_7 */
+	// MUX_GATE(CLK_TOP_MUX_VDEC, "vdec_sel", vdec_parents,
+	// 	0x015C, 8, 4, 15),
+	// MUX_GATE(CLK_TOP_MUX_DPILVDS, "dpilvds_sel", dpilvds_parents,
+	// 	0x015C, 24, 3, 31),
+	//
+	// /* CLK_CFG_8 */
+	// MUX_GATE(CLK_TOP_MUX_PMICSPI, "pmicspi_sel", pmicspi_parents,
+	// 	0x0164, 0, 3, 7),
+	// MUX_GATE(CLK_TOP_MUX_MSDC0, "msdc0_sel", msdc0_parents,
+	// 	0x0164, 8, 3, 15),
+	// MUX_GATE(CLK_TOP_MUX_SMI_MFG_AS, "smi_mfg_as_sel", smi_mfg_as_parents,
+	// 	0x0164, 16, 2, 23),
 };
 
 static const struct mtk_clk_desc topck_desc = {
