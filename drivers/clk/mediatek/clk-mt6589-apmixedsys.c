19b3ceeb1725c587bc372af339b2d53627b841d4
Tsuyukusa Akari (-akku-) <akkun11.open@gmail.com>
2025-12-28 03:04:19 +0900

    clk: mediatek: mt6589-apmixedsys: set pll fmax from tooniis port

--- drivers/clk/mediatek/clk-mt6589-apmixedsys.c
+++ drivers/clk/mediatek/clk-mt6589-apmixedsys.c
@@ -51,12 +51,11 @@
 
 #define VOID_REG	0x0
 
-/* FIXME: MT6589 config */
-#define MT8590_PLL_FMAX		(2000 * MHZ)
 #define CON0_MT6589_RST_BAR	BIT(27)
 
-#define PLL(_id, _name, _reg, _pwr_reg, _en_mask, _flags, _pcwbits, \
-			_pd_reg, _pd_shift, _pcw_reg, _pcw_shift) {	\
+#define PLL(_id, _name, _reg, _pwr_reg, _en_mask, _flags, _pcwbits,	\
+			_pd_reg, _pd_shift, _pcw_reg, _pcw_shift,	\
+			_fmax) {					\
 		.id = _id,						\
 		.name = _name,						\
 		.reg = _reg,						\
@@ -64,7 +63,7 @@
 		.en_mask = _en_mask,					\
 		.flags = _flags,					\
 		.rst_bar_mask = CON0_MT6589_RST_BAR,			\
-		.fmax = MT8590_PLL_FMAX,				\
+		.fmax = _fmax,						\
 		.pcwbits = _pcwbits,					\
 		.pd_reg = _pd_reg,					\
 		.pd_shift = _pd_shift,					\
@@ -75,21 +74,21 @@
 
 static const struct mtk_pll_data plls[] = {
 	PLL(CLK_APMIXED_ARMPLL, "armpll", ARMPLL_CON0, ARMPLL_PWR_CON0, 0x80000001,
-		PLL_AO, 21, ARMPLL_CON1, 24, ARMPLL_CON1, 0),
+		PLL_AO, 21, ARMPLL_CON1, 24, ARMPLL_CON1, 0, 1300000000),
 	PLL(CLK_APMIXED_MAINPLL, "mainpll", MAINPLL_CON0, MAINPLL_PWR_CON0, 0xf0000001,
-		HAVE_RST_BAR, 21, MAINPLL_CON0, 6, MAINPLL_CON1, 0),
+		HAVE_RST_BAR, 21, MAINPLL_CON0, 6, MAINPLL_CON1, 0, 1612000000),
 	PLL(CLK_APMIXED_MSDCPLL, "msdcpll", MSDCPLL_CON0, MSDCPLL_PWR_CON0, 0x80000001,
-		0, 21, MSDCPLL_CON0, 6, MSDCPLL_CON1, 0),
+		0, 21, MSDCPLL_CON0, 6, MSDCPLL_CON1, 0, 1664000000),
 	PLL(CLK_APMIXED_TVDPLL,  "tvdpll",  TVDPLL_CON0, TVDPLL_PWR_CON0, 0x80000001,
-		0, 21, TVDPLL_CON0, 6, TVDPLL_CON1, 0),
+		0, 21, TVDPLL_CON0, 6, TVDPLL_CON1, 0, 2376000000),
 	PLL(CLK_APMIXED_LVDSPLL, "lvdspll", LVDSPLL_CON0, LVDSPLL_PWR_CON0, 0x80000001,
-		0, 21, LVDSPLL_CON0, 6, LVDSPLL_CON1, 0),
+		0, 21, LVDSPLL_CON0, 6, LVDSPLL_CON1, 0, 1300000000),
 	PLL(CLK_APMIXED_UNIVPLL, "univpll", UNIVPLL_CON0, VOID_REG, 0xf3000001,
-		HAVE_RST_BAR, 7, UNIVPLL_CON0, 6, UNIVPLL_CON0, 8),
+		HAVE_RST_BAR, 7, UNIVPLL_CON0, 6, UNIVPLL_CON0, 8, 1248000000),
 	PLL(CLK_APMIXED_MMPLL, "mmpll", MMPLL_CON0, VOID_REG, 0xf0000001,
-		HAVE_RST_BAR, 7, MMPLL_CON0, 6, MMPLL_CON0, 8),
+		HAVE_RST_BAR, 7, MMPLL_CON0, 6, MMPLL_CON0, 8, 1430000000),
 	PLL(CLK_APMIXED_ISPPLL, "isppll", ISPPLL_CON0, VOID_REG, 0x80000001,
-		0, 7, MMPLL_CON0, 7, MMPLL_CON0, 8),
+		0, 7, MMPLL_CON0, 7, MMPLL_CON0, 8, 1664000000),
 };
 
 /* init logic from MT2712 */
