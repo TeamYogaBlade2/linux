8577ea9619d9b394769fbbdee0470b7f5da827c3
Akari Tsuyukusa <akkun11.open@gmail.com>
2026-03-14 23:07:02 +0900

    soc: mediatek: mmsys: init mt6589 support
    everyone will say "fixme"
    
    Signed-off-by: Akari Tsuyukusa <akkun11.open@gmail.com>

---
+++ drivers/soc/mediatek/mt6589-dispsys.h
@@ -0,0 +1,15 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2026 Akari Tsuyukusa <akkun11.open@gmail.com>
+ */
+
+#ifndef __SOC_MEDIATEK_MT6589_DISPSYS_H
+#define __SOC_MEDIATEK_MT6589_DISPSYS_H
+
+#include "mtk-mmsys.h"
+
+static const struct mtk_mmsys_routes mt6589_dispsys_routing_table = {
+	/* TODO */
+};
+
+#endif /* __SOC_MEDIATEK_MT6589_DISPSYS_H */
