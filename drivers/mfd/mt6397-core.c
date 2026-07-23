6917d0987c5d59441a8ccd135c917bc2e2ac5952
Roman Vivchar <rva333@protonmail.com>
2026-07-23 17:28:49 +0300

    mfd: mt6397: add mt6323 ACCDET support
    The mt6323 PMIC includes an ACCDET. Register the ACCDET in the mt6323
    devices array to allow the corresponding driver to probe using compatible
    string.
    
    Signed-off-by: Roman Vivchar <rva333@protonmail.com>

--- drivers/mfd/mt6397-core.c
+++ drivers/mfd/mt6397-core.c
@@ -174,6 +174,9 @@ static const struct mfd_cell mt6320_devs[] = {
 
 static const struct mfd_cell mt6323_devs[] = {
 	{
+		.name = "mt6323-accdet",
+		.of_compatible = "mediatek,mt6323-accdet",
+	}, {
 		.name = "mt6323-auxadc",
 		.of_compatible = "mediatek,mt6323-auxadc",
 	}, {
